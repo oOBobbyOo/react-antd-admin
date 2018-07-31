bd521fbf3e3f0d1864bb535cedf1f4f5fe269dec
Bobby <944831575@qq.com>
2018-07-31 10:50:17 +0800

    change routes _import_views

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,32 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import AuthenticatedRoute from '@/components/Authorized/AuthenticatedRoute'
+import asyncComponent from '@/components/AsyncComponent'
 
-import Layout from '@/views/Layout'
-import Login from '@/views/Login'
-import NotAuth from '@/views/Errors/401'
-import NotFound from '@/views/Errors/404'
+// import Layout from '@/views/Layout'
+// import Login from '@/views/Login'
+// import NotAuth from '@/views/Errors/401'
+// import NotFound from '@/views/Errors/404'
+// import Home from '@/views/Home'
+// import Iconfont from '@/views/UIElement/Iconfont'
+// import User from '@/views/User'
+// import BasicTable from '@/views/Table/BasicTable'
+// import AdvancedTable from '@/views/Table/AdvancedTable'
+// import Echarts from '@/views/Charts/Echarts'
 
-import Home from '@/views/Home'
-import Iconfont from '@/views/UIElement/Iconfont'
+const _import_views = file => asyncComponent(() => import(`@/views/${file}`))
+// const Layout = asyncComponent(() => import('@/views/Layout'))
 
-import User from '@/views/User'
-import BasicTable from '@/views/Table/BasicTable'
-import AdvancedTable from '@/views/Table/AdvancedTable'
-
-import Echarts from '@/views/Charts/Echarts'
+const Layout = _import_views('Layout')
+const Login = _import_views('Login')
+const NotAuth = _import_views('Errors/401')
+const NotFound = _import_views('Errors/404')
+const Home = _import_views('Home')
+const Iconfont = _import_views('UIElement/Iconfont')
+const User = _import_views('User')
+const BasicTable = _import_views('Table/BasicTable')
+const AdvancedTable = _import_views('Table/AdvancedTable')
+const Echarts = _import_views('Charts/Echarts')
 
 export const childRoutes = [
   {
